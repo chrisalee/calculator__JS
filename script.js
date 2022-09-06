a0d74e4b7fe2acbd1253a7d4fc17b5fdc389fe7a
Christopher L <56335217+chrisalee@users.noreply.github.com>
2022-09-06 16:01:15 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+let buttons = document.querySelectorAll('button');
+let erasebtn = document.querySelector('#erase');
+let clearbtn = document.querySelector('#clear');
+let evaluate = document.querySelector('#evaluate');
+let realTimeScreenValue = [];
+
+clearbtn.addEventListener('click', () => {
+  realTimeScreenValue = [''];
+  answerScreen.innerHTML = 0;
+  currentInput.className = 'currentInput';
+  answerScreen.className = 'answerScreen';
+  answerScreen.style.color = 'rgba(150, 150, 150, 0.87)';
+});
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    
+  //when clicked button is not earsed button
+    if(!btn.id.match('erase')) {
+      //to display value on button press
+      realTimeScreenValue .push(btn.value);
+      currentInput.innerHTML = realTimeScreenValue.join('');
+      //to evaluate answer in real time
+      if(btn.classList.contains('num_btn')) {
+        answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
+      }
+    }
+  //when erase button is clicked
+    if(btn.id.match('erase')) {
+      realTimeScreenValue.pop();
+      currentInput.innerHTML = realTimeScreenValue.join('');
+      answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
+    }
+  //when clicked button is evaluate button
+    if(btn.id.match('evaluate')) {
+      currentInput.className = 'answerScreen';
+      answerScreen.className = 'currentInput';
+      answerScreen.style.color = '#ffffff';
+    }
+  //to prevent undefined error in screen
+    if(typeof eval(realTimeScreenValue.join('')) == 'undefined') {
+      answerScreen.innerHTML = 0;
+    }
+  })
+})
